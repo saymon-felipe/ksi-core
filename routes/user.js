3f2e6f98454100c33c0acb823832361c60ad1860
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2025-08-27 23:24:20 -0300

    Refresh do token ao acessar o get de users

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const login = require("../middleware/login");
 const _usersService = require("../services/usersService");
 const functions = require("../utils/functions");
+const jwt = require('jsonwebtoken');
 
 router.post("/login", (req, res, next) => {
     _usersService.login(req.body.token).then((results) => {
         res.cookie('jwtToken', results.token, {
-            httpOnly: true, // Isso torna o cookie inacessível via JavaScript
+            httpOnly: true, 
             secure: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test', // Use 'secure' apenas em HTTPS
             sameSite: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test' ? 'None' : 'strict',
-            maxAge: 28800000 // Tempo de expiração do cookie em milissegundos (8 horas)
+            maxAge: 28800000
         });
 
         let response = functions.createResponse("Usuário autenticado com sucesso", results.user, "POST", 200);
@@ -21,6 +22,24 @@ router.post("/login", (req, res, next) => {
 });
 
 router.get("/", login, (req, res, next) => {
+    let token = jwt.sign({
+        id: req.usuario.id,
+        nome: req.usuario.nome,
+        email: req.usuario.email,
+        imagem: req.usuario.imagem
+    }, 
+    process.env.JWT_KEY,
+    {
+        expiresIn: "8h"
+    })
+
+    res.cookie('jwtToken', token, {
+        httpOnly: true, 
+        secure: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test', // Use 'secure' apenas em HTTPS
+        sameSite: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test' ? 'None' : 'strict',
+        maxAge: 28800000
+    });
+
     let response = functions.createResponse("Retorno do usuário", req.usuario, "GET", 200);
     return res.status(200).send(response);
 })
