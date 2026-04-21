cd61079a9c2fd8586744f831903b9e0cfdc2e8a9
Saymon <linnubr@gmail.com>
2026-04-21 01:19:25 -0300

    Aumento do timeout

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const app = require('./app');
 const port = process.env.PORT || 3001;
 const server = http.createServer(app);
 
+server.setTimeout(300000);
+server.keepAliveTimeout = 300000;
+server.headersTimeout = 301000;
+
 server.listen(port);
